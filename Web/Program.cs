bca9dee3ca6aed3f20c4107680e0c5f2890f4494
Kuplyaev Aleksey <a.kuplyaev@gmail.com>
2016-06-01 15:49:51 +0300

    Консольное приложение

---
+++ Web/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Web
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adress = "http://localhost:9081";
+            using (Microsoft.Owin.Hosting.WebApp.Start<Startup1>(url:adress))
+            {
+                Console.WriteLine(adress);
+                Console.ReadLine();
+            }
+          
+        }
+    }
+}
---
+++ Web/Startup1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Web.Startup1))]
+
+namespace Web
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/html; charset=utf-8";
+                return context.Response.WriteAsync("Hello world");
+            });
+        }
+    }
+}
