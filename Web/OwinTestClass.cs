288612eb0a8e6949042635d56eb9f9ae9843e39f
Aleksey Kuplyaev <a.kuplyaev@gmail.com>
2016-06-04 02:47:26 +0300

    OwiinTypes

--- Web/OwinTestClass.cs
+++ Web/OwinTestClass.cs
@@ -1,25 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace Web {   
-    using System.IO;
+namespace Web {
+    using Microsoft.Owin;
     using AppFunc = Func<IDictionary<string, object>, Task>;
     class HelloTest {
         private readonly AppFunc _next;
         public HelloTest(AppFunc next) {
             _next = next;
         }
-        
-        public  Task Invoke(IDictionary<string, object> env)
-        {         
-            var response = env["owin.ResponseBody"] as Stream;
-            using (var writer = new StreamWriter(response)) {
-                return writer.WriteAsync("Hello World");
-            }
+
+        public Task Invoke(IDictionary<string, object> env) {
+            var response = new OwinResponse(env);
+            return response.WriteAsync("Hello World");
+
         }
     }
-      
 }
+
+
